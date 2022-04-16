f89602c6512a41ea629c6939ff59eea73ce628be
Stanmegga <ezeokestanley.ifeanyi@gmail.com>
2022-04-16 12:35:14 +0100

    Add Javascript pop-up functionality to project

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ const projects = [
   {
     id: 'mini-project-btn6',
     title: 'Website Portfolio',
-    image: 'images/ImgPlaceholder3.png',
+    image: 'images/Snapshoot-Portfolio.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
     technologies: ['html', 'bootstrap', 'Ruby'],
